1a929ae32baef781b3ad2e7440caf1cb1024034e
barthelemy-drabczuk <barth.drabczuk@gmail.com>
2021-06-03 15:06:18 +0200

    ajout des fonctions de gestions d'utilisateurs

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const session = require('express-session');
 const app = express();
 const Twig = require('twig');
 const { Client } = require('pg');
@@ -66,11 +65,7 @@ let getUtilisateursParStatut = function (statut) {
 // fin partie utilisateur
 
 app.get('/', (req, res) => {
-  if(!req.session.user || !req.session.password)
-    res.redirect("/login")
-  else {
-    res.render("home/home_index.html.twig", {login:req.session.user});
-  }
+  res.render("home/home_index.html.twig", {});
 });
 
 app.get('/login', (req, res) => {
@@ -81,126 +76,30 @@ app.get('/login', (req, res) => {
   });
 });
 
-app.post('/login', (req, res) => {
-  let errorMsg = "";
-  let id = req.body.id;
-  let password = req.body.password;
-
-  if(id !== "" && password !== "") {
-    let sqlReq = "SELECT * FROM Utilisateur WHERE identifiant=$1;";
-    let values = [id];
-
-    client.query(sqlReq, values, (err, resp) => {
-      const result = err ? err.stack : resp.rows;
-
-      if(result === undefined || result[0] == undefined || !passwordHash.verify(password, result[0].mdp))
-        res.render("connection/connection_login.html.twig", {error:"Identifiant ou mot de passe incorrect"});
-      else{
-        req.session.user = result[0].identifiant;
-        req.session.password = result[0].mdp;
-        res.redirect("/");
-      }
-    });
-  } else
-    res.render("connection/connection_login.html.twig", {error:"L'identifiant et le mot de passe doivent être définis"});
-
-});
-
 app.get('/register', (req, res) => {
     res.render("connection/connection_register.twig", {});
 });
 
-app.post('/register', (req, res) => {
-  let errorMsg = "";
-  let id = req.body.id;
-  let password = req.body.password;
-
-  if(id !== "" && password !== "") {
-    let existsSqlReq = "SELECT * FROM Utilisateur WHERE identifiant=$1;";
-    let existsValues = [id];
-
-    client.query(existsSqlReq, existsValues, (err, resp) => {
-      const existsResult = err ? err.stack : resp.rows[0];
-
-      if(existsResult === undefined) {
-        let sqlReq = "INSERT INTO Utilisateur(identifiant, mdp, statut) values($1, $2, $3);";
-        let values = [id, passwordHash.generate(password), 0];
-
-        client.query(sqlReq, values, (err, resp) => {
-          const result = err ? err.stack : resp.rows[0];
-
-          if(result === undefined)
-            res.redirect("/login");
-          else
-            res.render("connection/connection_register.twig", {error:"Impossible de créer le compte"});
-        });
-      } else
-        res.render("connection/connection_register.twig", {error:"L'utilisateur " + id + " existe déjà"});
-    });
-  } else
-    res.render("connection/connection_register.twig", {error:"L'identifiant et le mot de passe doivent être définis"});
-});
-
 app.get('/ingredient', (req, res) => {
-  if(!req.session.user || !req.session.password)
-    res.redirect("/login")
-  else {
-    var userId = req.session.user;
-    var sqlReq = "SELECT DISTINCT Ingredient.nom, Ingredient.unite, Stocker.quantite FROM Ingredient, Stocker WHERE Ingredient.id IN (SELECT id_ingredient FROM Stocker WHERE identifiant_utilisateur=$1) AND Stocker.quantite = (SELECT DISTINCT quantite from Stocker where id_ingredient = Ingredient.id);"
-    var sqlReqUnites = "SELECT DISTINCT unite FROM Ingredient;"
+  var sqlReq = "SELECT * FROM Ingredient;"
+  var sqlReqUnites = "SELECT DISTINCT unite FROM Ingredient;"
+  client.query(sqlReq, (err, resp) => {
+    client.query(sqlReqUnites, (erru, respu) => {
+      var result = err ? err.stack : resp.rows;
+      var resultU = erru ? erru.stack : respu.rows;
 
-    var sqlIngParam = [req.session.user];
-    client.query(sqlReq, sqlIngParam, (err, resp) => {
-      client.query(sqlReqUnites, (erru, respu) => {
-        var result = err ? err.stack : resp.rows;
-        var resultU = erru ? erru.stack : respu.rows;
-
-        res.render('ingredient/ingredient_index.html.twig', {login:req.session.user, data:result, unites:resultU});
-      });
+      res.render('ingredient/ingredient_index.html.twig', {data:result, unites:resultU});
     });
-  }
-});
-
-app.post('/ingredient', (req, res) => {
-  if(!req.session.user || !req.session.password)
-    res.redirect("/login")
-
-  else {
-    var ingredient = req.body.name;
-    var quantity = req.body.quantity;
-    var unite = req.body.unite;
-
-    var sqlReq = "INSERT INTO Stocker(identifiant_utilisateur, id_ingredient, quantite, date_stock) VALUES($1, (SELECT id FROM Ingredient WHERE nom=$2), $3, $4)";
-    var values = [req.session.user, ingredient, quantity, Date.now()];
-
-    client.query(sqlReq, values, (err, resp) => {
-      const result = err ? err.stack : resp.rows[0];
-
-      res.redirect('/ingredient');
-    });
-  }
+  });
 });
 
 app.get('/recettes', (req, res) => {
-  if(!req.session.user || !req.session.password)
-    res.redirect("/login")
-  else {
-    var sqlReq = "SELECT * FROM Ingredient;"
-    client.query(sqlReq, (err, resp) => {
-      var result = err ? err.stack : resp.rows;
-      res.render('recipe/recipe_index.html.twig',{login:req.session.user, data:result});
-    });
-  }
+  var sqlReq = "SELECT * FROM Ingredient;"
+  client.query(sqlReq, (err, resp) => {
+    var result = err ? err.stack : resp.rows;
+    res.render('recipe/recipe_index.html.twig', {data:result});
+  })
 });
-
-app.get("/logout",(req,res) =>{
-  if(!req.session.user || !req.session.password)
-    res.redirect("/login")
-
-  req.session.destroy(function(err) {});
-  res.redirect("/")
-
-})
 
 // Handle 404 - Keep this as a last route
 app.use(function(req, res, next) {
