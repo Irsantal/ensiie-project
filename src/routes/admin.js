29097e4ad955e5074ae9845c333cdd43bd30bf3f
Irvinn Rochette <irvinn.rochette@ensiie.fr>
2021-06-18 01:32:57 -0700

    affichage des roles et CSS + page d'admin dans le menu que pour les admin

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,31 +1,20 @@
 const { client } = require('./../db');
 
 module.exports.adminPageHandler = (req, res) => {
-    if(!req.session.user || !req.session.password)
-        res.redirect("/login")
+    if(!req.session.user || !req.session.password || req.session.role !=2)
+        res.redirect("/")
     else {
-        let isAdmin = 'SELECT statut FROM utilisateur WHERE identifiant=$1 AND mdp=$2'
-        let values = [req.session.user,req.session.password];
-        client.query(isAdmin, values, (err, resp) => {
-            result = err ? err.stack : resp.rows;
-            if (result[0].statut !== 2) {
-                res.redirect('/');
-            }
-            else {
-                var tousLesUtilisateurs = 'SELECT * FROM utilisateur';
-                client.query (tousLesUtilisateurs, (err, resp) => {
-                    utilResult = err ? err.stack : resp.rows;
-
-                    res.render("admin/admin.html.twig", {utilisateurs: utilResult});
-                })
-            }
-        });
+          var tousLesUtilisateurs = 'SELECT * FROM utilisateur';
+          client.query (tousLesUtilisateurs, (err, resp) => {
+              utilResult = err ? err.stack : resp.rows;
+              res.render("admin/admin.html.twig", {login: req.session.user,utilisateurs: utilResult, isAdmin: (req.session.role ==2)});
+          });
     }
 };
 
-module.exports.changeRights = (res, req) => {
-    if(!req.session.user || !req.session.password)
-        res.redirect("/login");
+module.exports.changeRights = (req, res) => {
+    if(!req.session.user || !req.session.password || req.session.role !=2)
+        res.redirect("/");
     else {
         let newRights = 'UPDATE utilisateur SET statut=$1 WHERE identifiant=$2';
         let values = [req.body.role, req.body.utilisateurid];
--- src/routes/home.js
+++ src/routes/home.js
@@ -2,6 +2,6 @@ module.exports.homeRequestHandler = (req, res) => {
   if(!req.session.user || !req.session.password)
     res.redirect("/login")
   else {
-    res.render("home/home_index.html.twig", {login:req.session.user});
+    res.render("home/home_index.html.twig", {login:req.session.user,isAdmin: (req.session.role ==2)});
   }
 }
